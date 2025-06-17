48bf665242dc48c481a255ca9d99a88dedb39846
Jake2001 <jsamh12@outlook.com>
2025-06-17 13:27:22 -0400

    Added the routes for the contact and about page

--- portfolio-web/src/App.js
+++ portfolio-web/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from "./components/Header"
 import HomeContent from "./components/HomeContent"
 import AboutContent from "./components/AboutContent"
+import ContactContent from "./components/ContactContent"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         {/* Genral Routes*/}
         <Route path="/" element={<HomeContent />} />
         <Route path="/about" element={<AboutContent />} />
+        <Route path="/contact" element={<ContactContent />} />
       </Routes>
     </Router>
       
