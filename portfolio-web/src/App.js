0497c4bd62d60b7cb5fd7f0b3829779c22148d94
Jake2001 <jsamh12@outlook.com>
2025-06-16 23:20:03 -0400

    Added Routes for the home and about page

--- portfolio-web/src/App.js
+++ portfolio-web/src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from "./components/Header"
+import HomeContent from "./components/HomeContent"
+import AboutContent from "./components/AboutContent"
 
 function App() {
   return (
-    <Header/>
+    <>
+    <Router>
+      <Header/>
+      <Routes>
+        {/* Genral Routes*/}
+        <Route path="/" element={<HomeContent />} />
+        <Route path="/about" element={<AboutContent />} />
+      </Routes>
+    </Router>
+      
+    </>
   )
 }
 
