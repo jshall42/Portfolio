246fabad97e24246ef3427ab1a8e096ea878d02f
Jake2001 <jsamh12@outlook.com>
2025-06-17 13:26:16 -0400

    Added the route for the contact page

--- portfolio-web/src/components/Header.jsx
+++ portfolio-web/src/components/Header.jsx
@@ -38,7 +38,7 @@ function Header() {
         <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link to="/" className="block py-2 px-3 text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">
+              <Link to="/" className="block py-2 px-3 text-gray-900 hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">
                 Home
               </Link>
             </li>
@@ -53,9 +53,9 @@ function Header() {
               </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">
+              <Link to="/contact" className="block py-2 px-3 text-gray-900 hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500">
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
