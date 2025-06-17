e5d98172d5a4e185ebc8f5d93655b0e49fa81e97
Jake2001 <jsamh12@outlook.com>
2025-06-16 23:21:59 -0400

    Added the home content page that shows users a picture of me and my resume to start the site off of

---
+++ portfolio-web/src/components/HomeContent.jsx
@@ -0,0 +1,28 @@
+import "../index.css"
+
+function HomeContent() {
+    return (
+        <div className="flex flex-col md:flex-row min-h-screen">
+        {/* Left side: Resume */}
+        <div className="w-full md:w-1/2 bg-white h-[600px] md:h-screen">
+            <iframe
+            src="/JacobHall_Resume.pdf"
+            className="w-full h-full"
+            title="Resume PDF"
+            style={{ border: 'none' }}
+            />
+        </div>
+
+        {/* Right side: Picture */}
+        <div className="w-full md:w-1/2 bg-gray-100 h-[600px] md:h-screen flex justify-center items-center">
+            <img
+            src="/pictures/DSC_0664.JPG"
+            alt="Jacob Hall"
+            className="max-w-full max-h-full object-cover rounded-md shadow-lg"
+            />
+        </div>
+        </div>
+    )
+}
+
+export default HomeContent
